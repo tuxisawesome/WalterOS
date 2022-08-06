c0e6d562d628e622e63e94d68aa2b681d17af728
Walter Brobson <walter.brobson@gmail.com>
2022-08-06 18:32:52 -0400

    Comit me

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -36,12 +36,12 @@ Framebuffer* InitializeGOP(){
 
 	status = uefi_call_wrapper(BS->LocateProtocol, 3, &gopGuid, NULL, (void**)&gop);
 	if(EFI_ERROR(status)){
-		Print(L"Unable to locate GOP\n\r");
+		Print(L"!!! Unable to locate GOP\n\r");
 		return NULL;
 	}
 	else
 	{
-		Print(L"GOP located\n\r");
+		Print(L"*   GOP located\n\r");
 	}
 
 	framebuffer.BaseAddress = (void*)gop->Mode->FrameBufferBase;
@@ -136,14 +136,14 @@ UINTN strcmp(CHAR8* a, CHAR8* b, UINTN length){
 
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	InitializeLib(ImageHandle, SystemTable);
-	Print(L"String blah blah blah \n\r");
+	Print(L"WalterOS Core Bootloader \n\r");
 
 	EFI_FILE* Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable);
 	if (Kernel == NULL){
-		Print(L"Could not load kernel \n\r");
+		Print(L"!!! Could not find kernel \n\r");
 	}
 	else{
-		Print(L"Kernel Loaded Successfully \n\r");
+		Print(L"*   Kernel Located Successfully \n\r");
 	}
 
 	Elf64_Ehdr header;
@@ -167,11 +167,11 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 		header.e_version != EV_CURRENT
 	)
 	{
-		Print(L"kernel format is bad\r\n");
+		Print(L"!!! Kernel format is bad\r\n");
 	}
 	else
 	{
-		Print(L"kernel header successfully verified\r\n");
+		Print(L"*   Kernel header successfully verified\r\n");
 	}
 
 	Elf64_Phdr* phdrs;
@@ -203,27 +203,22 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 		}
 	}
 
-	Print(L"Kernel Loaded\n\r");
+	Print(L"*   Kernel Loaded\n\r");
 	
 
 	PSF1_FONT* newFont = LoadPSF1Font(NULL, L"zap-light16.psf", ImageHandle, SystemTable);
 	if (newFont == NULL){
-		Print(L"Font is not valid or is not found\n\r");
+		Print(L"!!! Font is not valid or is not found\n\r");
 	}
 	else
 	{
-		Print(L"Font found. char size = %d\n\r", newFont->psf1_Header->charsize);
+		Print(L"*   Font found. char size = %d\n\r", newFont->psf1_Header->charsize);
 	}
 	
 
 	Framebuffer* newBuffer = InitializeGOP();
 
-	Print(L"Base: 0x%x\n\rSize: 0x%x\n\rWidth: %d\n\rHeight: %d\n\rPixelsPerScanline: %d\n\r", 
-	newBuffer->BaseAddress, 
-	newBuffer->BufferSize, 
-	newBuffer->Width, 
-	newBuffer->Height, 
-	newBuffer->PixelsPerScanLine);
+	
 
 	EFI_MEMORY_DESCRIPTOR* Map = NULL;
 	UINTN MapSize, MapKey;
@@ -260,7 +255,7 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	bootInfo.mMapSize = MapSize;
 	bootInfo.mMapDescSize = DescriptorSize;
 	bootInfo.rsdp = rsdp;
-
+	Print(L"Booting kernel...\n\r");
 	SystemTable->BootServices->ExitBootServices(ImageHandle, MapKey);
 
 	KernelStart(&bootInfo);
